04ee2e13a96fa3ff4cdb40a464329785c04fdd2d
CliconlineStudios <105317102+CliconlineStudios@users.noreply.github.com>
2022-07-13 23:14:18 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ function inicio() {
     document.getElementById("screen2").style.display = "none";
     document.getElementById("back").style.display = "none";
     document.getElementById("side").style.display = "none";
+    document.getElementById("screen3").style.display = "none";
+    document.getElementById("screen4").style.display = "none";
     conta=0;
     contador=0;
 }
@@ -15,16 +17,51 @@ var btn = document.getElementById("inicio"),
     contador=0;
 
 
-function catalogos() {
+function insumos() {
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("screen3").style.display = "block";
+        document.getElementById("back").style.display = "block";
+        document.getElementById("side").style.display = "none";
+        contador=1;
+        conta=0;
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+           }); 
+    }
 
-if (contador==0) {
+
+function infantil() {
+
+
     document.getElementById("screen1").style.display = "none";
     document.getElementById("screen2").style.display = "block";
+    document.getElementById("screen3").style.display = "none";
     document.getElementById("back").style.display = "block";
     document.getElementById("side").style.display = "none";
     contador=1;
+    conta=0;
 }
-}
+
+function contac() {
+
+ 
+        document.getElementById("screen1").style.display = "none";
+        document.getElementById("screen2").style.display = "none";
+        document.getElementById("back").style.display = "block";
+        document.getElementById("side").style.display = "none";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen4").style.display = "block";
+        contador=1;
+        conta=0;
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+           }); 
+    
+    }
+
 
 
 function back() {
@@ -33,11 +70,15 @@ function back() {
         document.getElementById("screen1").style.display = "block";
         document.getElementById("screen2").style.display = "none";
         document.getElementById("back").style.display = "none";
+        document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen2").style.display = "none";
         contador=0;
     }
     else{
         document.getElementById("screen2").style.display = "block";
         document.getElementById("screen3").style.display = "none";
+        document.getElementById("screen4").style.display = "none";
         document.getElementById("screen4").style.display = "none";
         contador=1;
     }
@@ -68,13 +109,11 @@ function back() {
            
        }
  
+// llevar cerrar side automatico
 
-    document.querySelector('.boton')   
-    .addEventListener('click',()=>{
-     window.scrollTo({
-     top: 0,
-     behavior: 'smooth'
-    });
+document.querySelector('.tarjeta').addEventListener('click',()=>{
+    document.getElementById("side").style.display = "none";
+    conta=0;
     });
    
     document.querySelector('.back')   
@@ -85,5 +124,88 @@ function back() {
     });
     });
 
+// formulario
 
+document.querySelector("#submit").addEventListener("click", e => {
+    e.preventDefault();
+  
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "593959907464";
+  
+    let cliente = document.querySelector("#cliente").value;
+    let venta = document.querySelector("#venta").value;
+    let productos = document.querySelector("#productos1").value;
+    let resp = document.querySelector("#respuesta");
+  
+    resp.classList.remove("fail");
+    resp.classList.remove("send");
+  
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+          *Mega Distribuidora Sánchez*%0A
+          *Datos del Contacto*%0A
+          *Nombre de Cliente*%0A
+          ${cliente}%0A
+          *Categoría*%0A
+          ${venta}%0A
+          *Busco*%0A
+          ${productos}%0A`;
+  
+    if (cliente === "" || venta === "" || productos === ""  ) {
+      resp.classList.add("fail");
+      resp.innerHTML = `Espera, ${cliente} faltan algunos Datos `;
+      return false;
+    }
+    resp.classList.remove("fail");
+    resp.classList.add("send");
+    resp.innerHTML = `Tu Msj Fue Enviada, ${cliente}`;
+  
+    window.open(url);
+  });
+  
+
+//ALERTAS>
+  function save(){
+    let url = `https://drive.google.com/u/0/uc?id=1F7gt7gsCsCUoeuyCLR4UXV5Ty2zncell&export=download`;
+    window.open(url); 
+   }
+  
+  function ftuser() {
+      Swal.fire({
+          title: 'Yesenia Sánchez',
+          text: 'Directora Ejecutiva',
+          imageUrl: 'img/Logo transp.Mega.png',
+          imageWidth: 150,
+          confirmButtonText: 'Añadir a Contactos',
+          showCloseButton: 'true',
+          showCancelButton: true,
+    
+      
+        }).then((result) => {
+          if (result.isConfirmed) {
+            save();
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Descarga Correcta',
+              showConfirmButton: false,
+              timer: 1500
+            })
+          }
+        })
+  
+   
+  };
+  
+  
+  
+  function msj2() {
+    Swal.fire({
+      icon: 'info',
+      title: 'Traducir a Ingles',
+      text: 'Opción Disponible Próximamente',
+      footer: 'Tarjeta De Presentación'
+    });
+  
+   
+  }
 
